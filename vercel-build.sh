07aa696e637364813affceaf2cea3d87262d0cb9
tfsomrat <themefisher.dev@gmail.com>
2022-08-24 13:58:36 +0600

    added vercel settings

---
+++ vercel-build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# generating json file
+echo "Generation Json..."
+node lib/jsonGenerator.js
+
+# generating theme screenshots
+echo "Generation Theme Screenshots..."
+node lib/screenshotThemes.js
+
+# generating resource screenshots
+echo "Generation Resource Screenshots..."
+node lib/screenshotResources.js
