46871bbb31fd892743d51243c705616ac842049b
tfsomrat <themefisher.dev@gmail.com>
2022-11-28 13:30:44 +0600

    added sitemap

---
+++ next-sitemap.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next-sitemap').IConfig} */
+const config = {
+  siteUrl: "https://zeon.studio/",
+  generateRobotsTxt: false,
+  generateIndexSitemap: false,
+  // more options: https://www.npmjs.com/package/next-sitemap
+};
+
+export default config;
