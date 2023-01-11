0e6f919dd8761d105cc3f89b83964bbcc7edac6b
tffaruk <faruk.themefisher@gmail.com>
2023-01-11 16:55:57 +0600

    add reload on logo click

--- layouts/components/Logo.js
+++ layouts/components/Logo.js
@@ -1,11 +1,25 @@
 import config from "@config/config.json";
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Logo = ({ src, height, width, className }) => {
+  const router = useRouter();
   // destructuring items from config object
   const { title, logo_height, logo_width } = config.site;
-  return (
+  return router.asPath === "/" ? (
+    <button
+      onClick={() => location.reload()}
+      className={`navbar-brand ${className} w-auto md:w-[${
+        logo_width.replace("px", "") + "px"
+      }]`}
+      style={{
+        height: logo_height.replace("px", "") + "px",
+      }}
+    >
+      <Image height={height} width={width} src={src} alt={title} priority />
+    </button>
+  ) : (
     <Link
       href="/"
       className={`navbar-brand ${className} w-auto md:w-[${
