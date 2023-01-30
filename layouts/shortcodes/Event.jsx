74b1ddb8d37c67fb6f61b7fad1ae6f82242a1e77
somrat sorkar <themefisher.dev@gmail.com>
2023-01-30 17:29:23 +0600

    added discord widget and fork button

--- layouts/shortcodes/Event.jsx
+++ layouts/shortcodes/Event.jsx
@@ -1,14 +1,14 @@
 const Event = ({ date, title, children }) => {
   return (
-    <li className="mb-8 ml-4">
-      <div className="absolute -left-1.5 mt-2 h-3 w-3 rounded-full border border-white bg-border dark:border-gray-900 dark:bg-darkmode-border"></div>
-      <time className="mb-1 text-sm font-normal leading-none text-text dark:text-darkmode-text">
-        {date}
-      </time>
+    <li className="mb-12 ml-4">
+      <div className="absolute -left-1.5 mt-2 h-3 w-3 rounded-full border border-white bg-gradient-to-r from-primary to-secondary dark:border-gray-900 dark:bg-darkmode-border"></div>
       <h3 className="mt-0 mb-1 text-lg font-semibold text-dark dark:text-darkmode-dark">
         {title}
       </h3>
-      <p className="mb-4 text-base font-normal text-text dark:text-darkmode-text">
+      <time className="mb-1 text-sm font-normal leading-none text-text dark:text-darkmode-text">
+        {date}
+      </time>
+      <p className="mb-4 mt-2 text-base font-normal text-text dark:text-darkmode-text">
         {children}
       </p>
     </li>
--- layouts/shortcodes/Timeline.jsx
+++ layouts/shortcodes/Timeline.jsx
@@ -1,8 +1,8 @@
 const Timeline = ({ children }) => {
   return (
-    <ol className="relative border-l border-border pl-3 dark:border-darkmode-border">
+    <ul className="relative list-none border-l border-border pl-0 dark:border-darkmode-border">
       {children}
-    </ol>
+    </ul>
   );
 };
 
