f21cbca764fc7322dfe3c67bbe2b50f4da00cf02
somrat sorkar <themefisher.dev@gmail.com>
2023-02-08 10:37:45 +0600

    returned community submitted themes

---
+++ content/themes/next-hext.md
@@ -0,0 +1,28 @@
+---
+title: Hext
+github: https://github.com/dewdot/hext
+demo: https://hext.vercel.app/
+author: Ravi
+author_link: https://dewcodes.com/
+author_twitter: DewDots
+date: 2022-09-21T00:00:00.000Z
+description: A web hosting template made using nextjs
+ssg:
+  - Next
+css:
+  - SCSS
+cms:
+  - Markdown
+category:
+  - Business
+draft: false
+publish_date: '2022-09-08T13:23:17Z'
+update_date: '2022-10-01T12:09:36Z'
+github_star: 8
+github_fork: 2
+---
+
+## Theme features
+
+- Fast
+- Fully Responsive
--- pages/themes/index.js
+++ pages/themes/index.js
@@ -2,6 +2,7 @@ import HomeCategory from "@components/HomeCategory";
 import Sidebar from "@components/Sidebar";
 import Themes from "@components/Themes";
 import config from "@config/config.json";
+import useFilterData from "@hooks/useFilterData";
 import usePricingFilter from "@hooks/usePricingFilter";
 import useThemesSort from "@hooks/useThemesSort";
 import Base from "@layouts/Baseof";
@@ -38,90 +39,38 @@ const Home = ({ frontmatter, cms, css, ssg, category, themes, tools }) => {
     arrayPremium,
     sortAsc,
   } = useFilterContext();
-
-  // theme filtering
-  const filterSSG = sortedThemes?.filter((theme) =>
-    arraySSG.length
-      ? arraySSG.find((type) =>
-          theme.frontmatter.ssg
-            ?.map((ssg) => slugify(ssg))
-            .includes(slugify(type))
-        )
-      : sortedThemes
-  );
-  const filterCMS = filterSSG?.filter((theme) =>
-    arrayCMS.length
-      ? arrayCMS.find((type) =>
-          theme.frontmatter.cms
-            ?.map((cms) => slugify(cms))
-            .includes(slugify(type))
-        )
-      : sortedThemes
-  );
-  const filterCSS = filterCMS?.filter((theme) =>
-    arrayCSS.length
-      ? arrayCSS.find((type) =>
-          theme.frontmatter.css
-            ?.map((css) => slugify(css))
-            .includes(slugify(type))
-        )
-      : sortedThemes
-  );
-  const filterCategory = filterCSS?.filter((theme) =>
-    arrayCategory.length
-      ? arrayCategory.find((type) =>
-          theme.frontmatter.category
-            ?.map((category) => slugify(category))
-            .includes(slugify(type))
-        )
-      : sortedThemes
-  );
-
-  const filterFree = filterCategory?.filter(
-    (theme) => !theme.frontmatter.price || theme.frontmatter.price < 0
-  );
-  const freeThemeByCategory = filterFree?.filter((theme) =>
-    arrayCategory.length
-      ? arrayCategory.find((type) =>
-          theme.frontmatter.category
-            ?.map((category) => slugify(category))
-            .includes(slugify(type))
-        )
-      : sortedThemes
-  );
-
-  const filterPremium = filterCategory?.filter(
-    (theme) => theme.frontmatter.price > 0
-  );
-
-  const premiumThemeByCategory = filterPremium?.filter((theme) =>
-    arrayCategory.length
-      ? arrayCategory.find((type) =>
-          theme.frontmatter.category
-            ?.map((category) => slugify(category))
-            .includes(slugify(type))
-        )
-      : sortedThemes
-  );
-
-  // handle filtered themes
-  const filteredThemes =
-    arrayFree.length > 0 && arrayPremium.length > 0
-      ? filterCategory
-      : arrayFree.length > 0
-      ? freeThemeByCategory
-      : arrayPremium.length > 0
-      ? premiumThemeByCategory
-      : filterCategory;
+  const filterFunction = (array, filterArray, params) => {
+    const filterData = array?.filter((theme) =>
+      filterArray.length
+        ? filterArray.find((type) =>
+            theme.frontmatter[params]
+              ?.map((data) => slugify(data))
+              .includes(slugify(type))
+          )
+        : sortedThemes
+    );
+    return filterData;
+  };
+  const filterSSG = filterFunction(sortedThemes, arraySSG, "ssg");
+  const filterCMS = filterFunction(filterSSG, arrayCMS, "cms");
+  const filterCSS = filterFunction(filterCMS, arrayCSS, "css");
+  const filterCategory = filterFunction(filterCSS, arrayCategory, "category");
 
   //  button for sorting
   const { sortMenu } = usePricingFilter(arrayFree, arrayPremium);
+  const { filteredThemes, filterFree, filterPremium } = useFilterData(
+    sortedThemes,
+    filterCategory,
+    arrayCategory,
+    arrayFree,
+    arrayPremium
+  );
+
   return (
     <Base
       title={frontmatter.title}
       meta_title={frontmatter.meta_title}
       description={frontmatter.description}
-      image={frontmatter.image}
       noindex={true}
     >
       <div className="flex" onClick={mouseHandler}>
@@ -130,13 +79,29 @@ const Home = ({ frontmatter, cms, css, ssg, category, themes, tools }) => {
           ssg={ssg}
           cms={cms}
           css={css}
-          themes={themesWithOthersCategory}
+          themes={
+            arrayPremium.length && arrayFree.length
+              ? sortedThemes
+              : arrayFree.length
+              ? arrayFree
+              : arrayPremium.length
+              ? arrayPremium
+              : sortedThemes
+          }
         />
         <main className="main">
           <div className="container">
             <div className="mb-8 block justify-between md:flex">
               <HomeCategory
-                themes={filteredThemes}
+                themes={
+                  arrayPremium.length && arrayFree.length
+                    ? sortedThemes
+                    : arrayFree.length
+                    ? arrayFree
+                    : arrayPremium.length
+                    ? arrayPremium
+                    : sortedThemes
+                }
                 category={category}
                 filterFree={filterFree}
                 filterPremium={filterPremium}
@@ -163,10 +128,10 @@ const Home = ({ frontmatter, cms, css, ssg, category, themes, tools }) => {
 
 export default Home;
 
-// for homepage data
+// for themeList data
 export const getStaticProps = async () => {
-  const themesList = await getListPage("content/themes/_index.md");
-  const { frontmatter } = themesList;
+  const themeList = await getListPage("content/themes/_index.md");
+  const { frontmatter } = themeList;
   const ssg = getSinglePage("content/ssg");
   const cms = getSinglePage("content/cms");
   const css = getSinglePage("content/css");
