93bf44392689a4791d159bcb50ae1b839352e833
somrat sorkar <themefisher.dev@gmail.com>
2023-12-05 11:28:53 +0600

    update theme ssr

--- pages/themes/[theme].js
+++ pages/themes/[theme].js
@@ -3,8 +3,6 @@ import ThemeInfo from "@/components/ThemeInfo";
 import ThemePreview from "@/components/ThemePreview";
 import Base from "@/layouts/Baseof";
 import MobileSidebar from "@/layouts/partials/MobileSidebar";
-import Themes from "@/layouts/Themes";
-import { similarItems } from "@/lib/utils/similarItems";
 import { markdownify, plainify } from "@/lib/utils/textConverter";
 import fs from "fs";
 import matter from "gray-matter";
@@ -13,7 +11,7 @@ import path from "path";
 const SingleTheme = ({ slug, theme, themes, authors }) => {
   const { frontmatter, content } = theme[0];
   const { title, description, meta_title, noindex, canonical } = frontmatter;
-  const similarThemes = similarItems(theme, themes, slug);
+  // const similarThemes = similarItems(theme, themes, slug);
 
   return (
     <Base
@@ -49,7 +47,7 @@ const SingleTheme = ({ slug, theme, themes, authors }) => {
               <ThemeInfo theme={theme} slug={slug} />
             </div>
           </div>
-          {similarThemes.length > 0 && (
+          {/* {similarThemes.length > 0 && (
             <div className="mt-24">
               <h2 className="mb-8 text-center">Similar Themes To Consider</h2>
               <Themes
@@ -59,7 +57,7 @@ const SingleTheme = ({ slug, theme, themes, authors }) => {
                 authors={authors}
               />
             </div>
-          )}
+          )} */}
         </div>
       </section>
     </Base>
@@ -104,66 +102,35 @@ export const getServerSideProps = async ({ params }) => {
   const { theme } = params;
 
   // get single page on server side
-  const getSinglePageServer = async (folder) => {
-    const filesPath = await new Promise((resolve, reject) => {
-      fs.readdir(path.join(process.cwd(), folder), (err, files) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(files);
-        }
-      });
+  const getSinglePageServer = async (folder, slug) => {
+    const pageData = await new Promise((resolve, reject) => {
+      fs.readFile(
+        path.join(process.cwd(), folder, `${slug}.md`),
+        "utf-8",
+        (err, data) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(data);
+          }
+        },
+      );
     });
-
-    const sanitizeFiles = filesPath.filter((file) => file.includes(".md"));
-    const filterSingleFiles = sanitizeFiles.filter((file) =>
-      file.match(/^(?!_)/),
-    );
-    const filesPromises = filterSingleFiles.map(async (filename) => {
-      const slug = filename.replace(".md", "");
-      const pageData = await new Promise((resolve, reject) => {
-        fs.readFile(
-          path.join(process.cwd(), folder, filename),
-          "utf-8",
-          (err, data) => {
-            if (err) {
-              reject(err);
-            } else {
-              resolve(data);
-            }
-          },
-        );
-      });
-      const pageDataParsed = matter(pageData);
-      const frontmatterString = JSON.stringify(pageDataParsed.data);
-      const frontmatter = JSON.parse(frontmatterString);
-      const content = pageDataParsed.content;
-      const url = frontmatter.url ? frontmatter.url.replace("/", "") : slug;
-
-      return { frontmatter: frontmatter, slug: url, content: content };
-    });
-
-    const singlePages = await Promise.all(filesPromises);
-    const publishedPages = singlePages.filter(
-      (page) => !page.frontmatter.draft && page,
-    );
-    const filterByDate = publishedPages.filter(
-      (page) => new Date(page.frontmatter.date || new Date()) <= new Date(),
-    );
-
-    return filterByDate;
+    const pageDataParsed = matter(pageData);
+    const frontmatterString = JSON.stringify(pageDataParsed.data);
+    const frontmatter = JSON.parse(frontmatterString);
+    const content = pageDataParsed.content;
+    return { frontmatter: frontmatter, slug: demo, content: content };
   };
 
-  const themes = await getSinglePageServer("content/themes");
-  const authors = await getSinglePageServer("content/authors");
-  const singleTheme = themes.filter((data) => data.slug === theme);
+  const singleTheme = await getSinglePageServer("content/themes", theme);
 
   return {
     props: {
       theme: singleTheme,
-      themes: themes,
       slug: theme,
-      authors: authors,
+      // themes: themes,
+      // authors: authors,
     },
   };
 };
