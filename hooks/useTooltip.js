7238ef8c5a1209bec42529677e155969afb3dccf
teamosishq <teamosishq@gmail.com>
2024-11-24 09:59:01 +0600

    update tooltip

--- hooks/useTooltip.js
+++ hooks/useTooltip.js
@@ -1,25 +1,28 @@
 import { useEffect } from "react";
 
-// tooltip
 const useTooltip = () => {
   useEffect(() => {
-    var tooltipEl = document.querySelectorAll(".tooltip");
-    if (tooltipEl) {
-      var tooltipItems = document.querySelectorAll(".tooltip-label");
-      tooltipItems.forEach((item) => {
-        item.remove();
-      });
-      var length = tooltipEl.length;
-      for (var i = 0; i < length; i++) {
-        var attr = tooltipEl[i].getAttribute("data-tooltip");
-        var x = document.createElement("SPAN");
-        var t = document.createTextNode(attr);
-        x.appendChild(t);
-        x.className = "tooltip-label";
-        tooltipEl[i].appendChild(x);
+    const tooltipElements = document.querySelectorAll(".tooltip");
+
+    tooltipElements.forEach((el) => {
+      if (!el.querySelector(".tooltip-label")) {
+        const tooltipText = el.getAttribute("data-tooltip");
+        if (tooltipText) {
+          const tooltipLabel = document.createElement("span");
+          tooltipLabel.textContent = tooltipText;
+          tooltipLabel.className = "tooltip-label";
+          el.appendChild(tooltipLabel);
+        }
       }
-    }
-  });
+    });
+
+    return () => {
+      tooltipElements.forEach((el) => {
+        const tooltipLabel = el.querySelector(".tooltip-label");
+        if (tooltipLabel) el.removeChild(tooltipLabel);
+      });
+    };
+  }, []);
 };
 
 export default useTooltip;
