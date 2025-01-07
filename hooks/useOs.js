ac126b2edd9dd9e44bfa25597a010c04c56ee87e
teamosishq <teamosishq@gmail.com>
2025-01-07 17:06:59 +0600

    added timezone data

--- hooks/useOs.js
+++ hooks/useOs.js
@@ -4,11 +4,14 @@ import { useEffect, useState } from "react";
 const useOs = () => {
   // get Os
   const [os, setOs] = useState(false);
+  const [platForm, setPlatForm] = useState("");
+
   useEffect(() => {
     setOs(navigator.platform.indexOf("Mac") > -1);
+    setPlatForm(navigator.platform);
   }, []);
 
-  return os;
+  return { os, platForm };
 };
 
 export default useOs;
