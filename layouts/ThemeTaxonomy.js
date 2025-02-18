ea1b59f433f34d237436d74a16a3fc1619fdac80
teamosishq <teamosishq@gmail.com>
2025-02-18 13:41:16 +0600

    added carbon ads to theme-category page

--- layouts/ThemeTaxonomy.js
+++ layouts/ThemeTaxonomy.js
@@ -3,6 +3,7 @@ import Themes from "@/layouts/Themes";
 import { markdownify, slugify } from "@/lib/utils/textConverter";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { TbBrandGithub, TbCode, TbHome, TbLicense } from "react-icons/tb";
+import CarbonAd from "./partials/CarbonAd";
 
 const { dark_icon_list } = config;
 
@@ -31,85 +32,88 @@ const ThemeTaxonomy = ({
       <section>
         <div className="container">
           <div
-            className={`mb-16 rounded p-6 shadow dark:bg-darkmode-theme-dark ${
+            className={`mb-16 flex flex-wrap md:flex-nowrap rounded p-6 shadow dark:bg-darkmode-theme-dark ${
               !showIntro && "hidden"
             }`}
           >
-            <div className="mb-5 flex">
-              <img
-                className={`${
-                  dark_icon_list.includes(slugify(title))
-                    ? "dark:brightness-0 dark:invert"
-                    : ""
-                } mr-3`}
-                src={icon}
-                alt={`${title} icon`}
-                height="40"
-                width="40"
-              />
-              {markdownify(
-                (page_title || title).replace("<themes>", `${themeCount}+`),
-                "h1",
-                "self-end",
-              )}
+            <div className="mb-4 mr-3 md:mb-0">
+              <div className="mb-5 flex">
+                <img
+                  className={`${
+                    dark_icon_list.includes(slugify(title))
+                      ? "dark:brightness-0 dark:invert"
+                      : ""
+                  } mr-3`}
+                  src={icon}
+                  alt={`${title} icon`}
+                  height="40"
+                  width="40"
+                />
+                {markdownify(
+                  (page_title || title).replace("<themes>", `${themeCount}+`),
+                  "h1",
+                  "self-end",
+                )}
+              </div>
+              {markdownify(content, "p", "mb-5")}
+              <ul className="meta-list">
+                {website && (
+                  <li title="Official Website">
+                    <TbHome />
+                    <a target="_blank" rel="noopener noreferrer" href={website}>
+                      {website}
+                    </a>
+                  </li>
+                )}
+
+                {github_path && (
+                  <li title="GitHub Repository">
+                    <TbBrandGithub />
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`https://github.com/${github_path}`}
+                    >
+                      {github_path}
+                    </a>
+                  </li>
+                )}
+
+                {twitter_username && (
+                  <li title="Twitter Profile">
+                    <FaSquareXTwitter />
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={`https://x.com/${twitter_username}`}
+                    >
+                      @{twitter_username}
+                    </a>
+                  </li>
+                )}
+
+                {license && (
+                  <li title="License">
+                    <TbLicense />
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      href={license_url}
+                    >
+                      {license}
+                    </a>
+                  </li>
+                )}
+
+                {language && (
+                  <li title="Language">
+                    <TbCode />
+                    {language}
+                  </li>
+                )}
+              </ul>
             </div>
-            {markdownify(content, "p", "mb-5")}
-            <ul className="meta-list">
-              {website && (
-                <li title="Official Website">
-                  <TbHome />
-                  <a target="_blank" rel="noopener noreferrer" href={website}>
-                    {website}
-                  </a>
-                </li>
-              )}
-
-              {github_path && (
-                <li title="GitHub Repository">
-                  <TbBrandGithub />
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={`https://github.com/${github_path}`}
-                  >
-                    {github_path}
-                  </a>
-                </li>
-              )}
-
-              {twitter_username && (
-                <li title="Twitter Profile">
-                  <FaSquareXTwitter />
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={`https://x.com/${twitter_username}`}
-                  >
-                    @{twitter_username}
-                  </a>
-                </li>
-              )}
-
-              {license && (
-                <li title="License">
-                  <TbLicense />
-                  <a
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    href={license_url}
-                  >
-                    {license}
-                  </a>
-                </li>
-              )}
-
-              {language && (
-                <li title="Language">
-                  <TbCode />
-                  {language}
-                </li>
-              )}
-            </ul>
+            <CarbonAd />
           </div>
 
           <Themes themes={data} authors={authors} />
